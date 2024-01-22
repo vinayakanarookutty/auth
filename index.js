b68ea89bbb76085a359f473debce8f86ef3f2674
Dilshad1041 <147979600+Dilshad1041@users.noreply.github.com>
2024-01-22 10:59:17 +0530

    added

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var express=require('express')
-var app=express()
-var bodyparser=require('body-parser')
-app.use(bodyparser.urlencoded({extended:true}))
-var home=require('./home')
-app.set('view engine','ejs')
-app.set('views','./views')
+var express = require("express");
+var app = express();
+var bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: true }));
+var home = require("./home");
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.listen("3000",()=>
-{
-    console.log("Server Started")
-})
-app.use("/",home)
+app.listen("3000", () => {
+  console.log("Server Started");
+});
+app.use("/", home);

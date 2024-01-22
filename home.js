85069e348dcc23e658019b07f083c7a26868cbc0
Dilshad1041 <147979600+Dilshad1041@users.noreply.github.com>
2024-01-22 12:15:43 +0530

    new endpoint for add student

--- home.js
+++ home.js
@@ -15,7 +15,13 @@ var userSchema = mongoose.Schema({
   email: String,
   password: String,
 });
+var studentSchema = mongoose.Schema({
+  registerNumber: String,
+  rollNumber: String,
+  studentName: String,
+});
 var UserModal = mongoose.model("user", userSchema);
+var StudentModel = mongoose.model("students", studentSchema);
 
 router.get("/", (req, res) => {
   res.render("LoginPage", { status: "ok" });
@@ -43,15 +49,34 @@ router.post("/login", async (req, res) => {
     res.render("LoginPage", { status: "UserName is Wrong" });
   }
 });
-router.post("/create-user", async (req, res) => {
+router.post("/register", async (req, res) => {
   var password = await bcrypt.hash(req.body.password, 10);
   var user = new UserModal({
-    userName: "Vinayak",
-    lastName: req.body.lastname,
+    universityName: req.body.universityName,
+    college: req.body.college,
+    department: req.body.department,
+    course: req.body.course,
+    Specialization: req.body.Specialization,
+    scheme: req.body.scheme,
+    semester: req.body.semester,
+    facultyName: req.body.facultyName,
     email: req.body.email,
     password: password,
   });
   user.save();
+  res.redirect("/");
+});
+
+router.get("/student", async (req, res) => {
+  res.render("AddStudents");
+});
+router.post("/student", async (req, res) => {
+  var student = new StudentModel({
+    registerNumber: req.body.registerNumber,
+    rollNumber: req.body.rollNumber,
+    studentName: req.body.studentName,
+  });
+  student.save();
   res.redirect("/");
 });
 
